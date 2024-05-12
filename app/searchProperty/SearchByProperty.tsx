220999a6af7cf59b98a264c8e5b71badb9117ec2
DigitalZeus <65643207+DigitalZeus@users.noreply.github.com>
2024-05-12 21:51:26 +0530

    final deploy

--- app/searchProperty/SearchByProperty.tsx
+++ app/searchProperty/SearchByProperty.tsx
@@ -1,10 +1,7 @@
 'use client'
 import React from 'react'
 import { useContext } from 'react'
-import { useRouter } from 'next/router'
 const SearchByProperty = () => {
-    const router = useRouter()
-    const { key, value } = router.query
   return (
     <div>SearchByProperty</div>
   )
